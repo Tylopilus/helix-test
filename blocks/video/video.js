30d496c22a345944a821a0a7546a2bc96bef3aa0
Helge <elitaer@gmail.com>
2023-07-11 22:14:37 +0200

    test

---
+++ blocks/video/video.js
@@ -0,0 +1,9 @@
+/*
+ * Video Block
+ * Show a video referenced by a link
+ * https://www.hlx.live/developer/block-collection/video
+ */
+
+export default async function decorate(block) {
+  console.log(block);
+}
